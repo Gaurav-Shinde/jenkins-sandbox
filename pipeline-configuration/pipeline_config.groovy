3f6a0eec2bd90f2c4b8ff5ee03b7d33dcf90c759
Gaurav-Shinde <gaurav.shinde@gmail.com>
2024-05-02 13:38:55 -0400

    reassigned shared-lib import

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,5 +1,5 @@
-@Library('shared-lib') _
-
+// removed due to error parsing - class not found and _ variable exists in current scope
+// @Library('shared-lib') _
 jte{
     pipeline_template = 'first-template.groovy'
 }
--- template-lib/greetingsV2/steps/greet.groovy
+++ template-lib/greetingsV2/steps/greet.groovy
@@ -1,4 +1,4 @@
-// @Library('shared-lib') _
+@Library('shared-lib') _
 
 void call(){
     stage("SayMessage: greet"){
