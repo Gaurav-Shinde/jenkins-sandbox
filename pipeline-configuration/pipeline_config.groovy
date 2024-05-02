ab871338f40faff85e862b225f20e75d9f5616c8
Gaurav-Shinde <gaurav.shinde@gmail.com>
2024-05-02 14:45:18 -0400

    step primitive overloading not allowed, unique steps per lib - need more config if same step across libs

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -6,6 +6,9 @@ jte{
     reverse_library_resolution = true
 }
 libraries{
+    // currently this is not permitted, leads to method overloading on primitive JTE exception with greet step. This may be possible to integrate with a build folder and steps under it and defining library config folder for optional params
+    // greetings
+    // greetingsV2
     greetings
     greetingsV2
 }
--- pipeline-templates/first-template.groovy
+++ pipeline-templates/first-template.groovy
@@ -1,3 +1,4 @@
 // ensure the template are under pipeline_templates directory (as specified in the job config) as this is what is searched by the Templating Engine plugin for named pipeline template https://boozallen.github.io/sdp-docs/jte/2.2.2/governance/pipeline_template_selection.html
 
 greet()
+greetV2()
